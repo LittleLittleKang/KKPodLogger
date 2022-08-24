16e1e9cc2195066b4ee4e3e8007a19d0ba7b8414
LittleLittleKang <tan452301@live.com>
2022-08-24 17:41:53 +0800

    更改httpRequest

--- KKPodLogger/Classes/KKPeople.m
+++ KKPodLogger/Classes/KKPeople.m
@@ -19,12 +19,19 @@
 
 - (void)httpRequest {
     
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    [manager GET:@"https://www.baidu.com/" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSLog(@"请求百度成功");
-    } failure:^(AFHTTPRequestOperation *operation, NSError*error) {
-        NSLog(@"请求百度失败");
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+
+    NSURL *URL = [NSURL URLWithString:@"http://example.com/download.zip"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+
+    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+        NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+        return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
+    } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+        NSLog(@"pod 下载结果");
     }];
+    [downloadTask resume];
 }
 
 @end
