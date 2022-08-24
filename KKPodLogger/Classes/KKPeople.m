2d9a65de153e23697d627eec727692ce67a066ce
LittleLittleKang <tan452301@live.com>
2022-08-24 16:47:43 +0800

    添加AFN

--- KKPodLogger/Classes/KKPeople.m
+++ KKPodLogger/Classes/KKPeople.m
@@ -6,12 +6,25 @@
 //
 
 #import "KKPeople.h"
+#import "AFNetworking.h"
 
 @implementation KKPeople
 
 - (void)sayHello {
     
     NSLog(@"KKPeople say hello");
+    
+    [self httpRequest];
+}
+
+- (void)httpRequest {
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager GET:@"https://www.baidu.com/" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"请求百度成功");
+    } failure:^(AFHTTPRequestOperation *operation, NSError*error) {
+        NSLog(@"请求百度失败");
+    }];
 }
 
 @end
